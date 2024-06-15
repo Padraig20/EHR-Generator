fc79c61ee7b70b5b502816378bc66738e841fe01
Patrick Styll <patrick.styll@tuwien.ac.at>
2024-06-15 22:35:24 +0200

    Add initial setup script for project setup

---
+++ initial_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Creating a new virtual environment..."
+python3 -m venv .venv
+
+echo "Activating the virtual environment..."
+source .venv/bin/activate
+
+echo "Installing dependencies from requirements.txt..."
+pip install -r requirements.txt
+
+echo "Moving into the 'data' directory..."
+cd data || { echo "Directory 'data' does not exist. Exiting."; exit 1; }
+
+echo "Downloading embeddings_icd.npy..."
+wget https://github.com/Padraig20/EHR-Generator/releases/download/Embeddings/embeddings_icd.npy
+
+echo "Downloading embeddings_ndc.npy..."
+wget https://github.com/Padraig20/EHR-Generator/releases/download/Embeddings/embeddings_ndc.npy
+
+echo "Setup complete. Move to the 'src' folder and invoke 'python api.py' to start the backend server."
+
