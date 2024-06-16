a8beb7ea324c3795c903b55a5f7984941bb36b17
Patrick Styll <patrick.styll@tuwien.ac.at>
2024-06-16 20:52:40 +0200

    quickfix setup script

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 echo -e "\e[94mCreating a new virtual environment...\e[0m"
-#python3 -m venv .venv
+python3 -m venv .venv
 
 echo -e "\e[94mActivating the virtual environment...\e[0m"
-#source .venv/bin/activate
+source .venv/bin/activate
 
 echo -e "\e[94mInstalling dependencies from requirements.txt...\e[0m"
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo -e "\e[94mDownloading spacy language model...\e[0m"
-#python -m spacy download en_core_web_sm
+python -m spacy download en_core_web_sm
 
 echo -e "\e[94mMoving into the 'data' directory...\e[0m"
-#cd data || { echo "Directory 'data' does not exist. Exiting."; exit 1; }
+cd data || { echo "Directory 'data' does not exist. Exiting."; exit 1; }
 
 echo -e "\e[94mDownloading embeddings_icd.npy...\e[0m"
-#wget https://github.com/Padraig20/EHR-Generator/releases/download/Embeddings/embeddings_icd.npy
+wget https://github.com/Padraig20/EHR-Generator/releases/download/Embeddings/embeddings_icd.npy
 
 echo -e "\e[94mDownloading embeddings_ndc.npy...\e[0m"
-#wget https://github.com/Padraig20/EHR-Generator/releases/download/Embeddings/embeddings_ndc.npy
+wget https://github.com/Padraig20/EHR-Generator/releases/download/Embeddings/embeddings_ndc.npy
 
 echo -e "\e[94mDownloading the necessary models...\e[0m"
 cd ..
